a6d7f360a34633a994ad26d390b717cea50a709c
annaely <annaely691@gmail.com>
2024-07-29 21:31:08 -0300

    Desafio-Projeto-Api

---
+++ src/test/java/me/dio/Dev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
